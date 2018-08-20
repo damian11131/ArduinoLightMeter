98edcb62fd61017281e936fa240e49a8f3a88e91
Damian <damian11131@gmail.com>
2018-08-20 14:08:35 +0200

    Add filename change as a command line arg.

--- ArduinoDataReceiver.py
+++ ArduinoDataReceiver.py
@@ -1,24 +1,38 @@
-import serial
+# A program that receives data from Arduino connected to port PORT_NAME.
+# Data is sent in up to 5 bytes packages including newline byte.
+# Newline character has to be sent at the end of data package
 
+
+import serial
+from sys import argv
+from sys import exit
 
 BAUD_RATE = 9600
 PORT_NAME = "COM3"
-FILENAME = "measures.txt"
+filename = "measures.txt"
 
 def main():
-    with serial.Serial(PORT_NAME, BAUD_RATE) as ser:
-        buff = ''
-        while True:
-            data = ser.readline().decode('utf-8').rstrip('r')
-            buff += data
+    global filename
 
-            count = 0
-            for i in range(len(buff)):
-                if buff[i] == '\n':
-                    count += 1
-            if count >= 5:
-                with open(FILENAME, 'w+') as measures:
-                    measures.write(buff)
+    if len(argv) >= 2:
+        filename = argv[1]
+
+    try:
+        with serial.Serial(PORT_NAME, BAUD_RATE) as ser:
+            buff = ''
+            while True:
+                data = ser.readline().decode('utf-8').rstrip('r')
+                buff += data
+
+                count = 0
+                for i in range(len(buff)):
+                    if buff[i] == '\n':
+                        count += 1
+                if count >= 5:
+                    with open(filename, 'w+') as measures:
+                        measures.write(buff)
+    except serial.SerialException:
+        exit("You didn't connect Arduino")
 
 
 if __name__ == "__main__":
